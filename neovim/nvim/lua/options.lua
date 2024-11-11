35997185bfc5889003869593d54e4086f237006b
Lucas Hartley <lucas@pololu.internal>
2024-11-11 14:23:06 -0800

    update bash and nvim config

--- neovim/nvim/lua/options.lua
+++ neovim/nvim/lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+local opt = vim.opt
+o.cursorlineopt ='both' -- to enable cursorline!
+opt.relativenumber = true;
