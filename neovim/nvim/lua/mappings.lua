987d9496e46e60023f782e125f27fb3d19f28e1d
Lucas Hartley <lucas@pololu.internal>
2024-11-13 13:41:27 -0800

    add persistence sessions

--- neovim/nvim/lua/mappings.lua
+++ neovim/nvim/lua/mappings.lua
@@ -7,5 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>ll", ":Limelight!!<CR>", { desc = "Toggle Limelight" })
+map("n", "<leader>qs", function() require("persistence").load() end, { desc = "Restore cwd session" }) -- load the session for the current directory
+map("n", "<leader>qS", function() require("persistence").select() end, { desc = "Select session" }) -- select a session to load
+map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, { desc = "Restore last session" }) -- load the last session
+map("n", "<leader>qd", function() require("persistence").stop() end, { desc = "Stop Persistence" }) -- stop Persistence => session won't be saved on exit
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- neovim/nvim/lua/plugins/init.lua
+++ neovim/nvim/lua/plugins/init.lua
@@ -45,6 +45,14 @@ return {
     "junegunn/limelight.vim",
     lazy = false,
   },
+
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    }
+  }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
