c7a75d9e896d9fee446c15428d9a07db928eedd3
Lucas Hartley <lucas@pololu.internal>
2024-11-13 13:41:47 -0800

    add mappings for moving lines up and down

--- neovim/nvim/lua/mappings.lua
+++ neovim/nvim/lua/mappings.lua
@@ -7,6 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>ll", ":Limelight!!<CR>", { desc = "Toggle Limelight" })
+map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 map("n", "<leader>qs", function() require("persistence").load() end, { desc = "Restore cwd session" }) -- load the session for the current directory
 map("n", "<leader>qS", function() require("persistence").select() end, { desc = "Select session" }) -- select a session to load
 map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, { desc = "Restore last session" }) -- load the last session
